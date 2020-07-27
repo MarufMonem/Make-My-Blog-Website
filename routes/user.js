6d8093823a5e78b299f0382b806087e3ecdd2705
Maruf Monem <marufmonem@gmail.com>
2020-07-27 12:57:43 +0600

    user routes redesigned

--- routes/user.js
+++ routes/user.js
@@ -2,25 +2,26 @@
 var express     = require("express");
 var router      = express.Router();
 var user        = require("../models/user");
+var blog        = require("../models/blog");
 var middleware  = require("../middleware");
 
 //create a new user req
-router.get("/create", function(req,res){
+router.get("/create", middleware.isloggedIn, function(req,res){
 
 });
 
 //Delete the user
-router.get("/delete/:id", function(req,res){
+router.get("/delete/:id", middleware.isloggedIn, function(req,res){
 
 });
 
 //Update the user req
-router.get("/update/:id", function(req,res){
+router.get("/update/:id", middleware.isloggedIn, function(req,res){
 
 });
 
 //Show users profile (containing user info, liked blogs and new post)
-router.get("/:id", function(req,res){
+router.get("/:id", middleware.isloggedIn, function(req,res){
 
 });
 
