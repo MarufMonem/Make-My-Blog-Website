c0c1fc3e375fde82aa0ce0e4d3ad0ffe6b89c88b
Anik <marufmonem@gmail.com>
2020-10-11 11:33:13 +0600

    faq model updated

--- models/faq.js
+++ models/faq.js
@@ -5,6 +5,7 @@ var faqSchema = new mongoose.Schema({
     title:  String,
     content:String,
     image:  String,
+    created: {type:Date, default:Date.now}
 });
 
 module.exports= mongoose.model("faq",faqSchema);
